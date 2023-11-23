25c9363f1a8dfeb9b10d7f18e6470cc30f0e6e5d
DoHyeon <wjs505@pi-lab.io>
2023-11-23 10:44:09 +0900

    feat: SBTContract.sol

---
+++ contracts/SBTContract.sol
@@ -0,0 +1,38 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import './ERC5484.sol';
+import './access/Ownable.sol';
+import './ISBTContract.sol';
+
+contract SBTContract is ERC5484, Ownable, ISBTContract {
+
+    uint private totalsupply = 0;
+    address private seller = address(0);
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable(_msgSender()) {}
+
+    function getTotalSupply() external view returns(uint) {
+        return totalsupply;
+    }
+
+    function getSeller() external view returns(address) {
+        return seller;
+    }
+
+    function setSeller(address _seller) external onlyOwner() {
+        seller = _seller;
+    }
+
+    function testMint(address to) external {
+        require(_msgSender() == seller, "Invalid seller");
+        totalsupply = totalsupply + 1;
+        _mintSBT(to, totalsupply, BurnAuth.OwnerOnly);
+    }
+
+    function testBurn(uint256 tokenId) external {
+        _burnSBT(tokenId);
+    }
+
+}
