84f7010c3c7dff9470e1ce9e690e1a4db129bbd5
DoHyeon <wjs505@pi-lab.io>
2023-11-24 17:14:17 +0900

    update: interfaces

--- contracts/interfaces/ISBTContract.sol
+++ contracts/interfaces/ISBTContract.sol
@@ -1,9 +1,8 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 interface ISBTContract {
-    function getTotalSupply() external view returns(uint);
-    function getSeller() external view returns(address);
     function setSeller(address _seller) external;
-    function testMint(address to) external;
-    function testBurn(uint256 tokenId) external;
+    function mintSBT(address to) external;
+    function burnSBT(uint256 tokenId) external;
 }
--- contracts/interfaces/ISBTPriceContract.sol
+++ contracts/interfaces/ISBTPriceContract.sol
@@ -1,6 +1,7 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 interface ISBTPriceContract {
-    function getBFCUSDCRatio() external view returns (uint, uint);
     function getPrice() external view returns(uint);
+    function getBFCUSDCRatio() external view returns (uint);
 }
