4ea574ac5b0ba2d54fc6ca734b90fffad487e529
DoHyeon <wjs505@pi-lab.io>
2023-11-28 13:47:14 +0900

    update: SaleContract.sol

--- contracts/SaleContract.sol
+++ contracts/SaleContract.sol
@@ -5,16 +5,13 @@ import "./interfaces/ISBTContract.sol";
 import "./interfaces/ISBTPriceContract.sol";
 import "./interfaces/IERC20.sol";
 import "./access/Ownable.sol";
-import "./utils/math/Math.sol";
 
 contract SaleContract is Ownable {
-    using Math for uint256;
 
     ISBTContract public tokenContract;
     ISBTPriceContract public priceContract;
     IERC20 public stableContract = IERC20(0x28661511CDA7119B2185c647F23106a637CC074f);
     bool public killSwitch = false;
-    uint256 public USDCDecimals = 10 ** 6;
 
     constructor() Ownable(_msgSender()) {
     }
@@ -32,7 +29,7 @@ contract SaleContract is Ownable {
         tokenContract = ISBTContract(_contract);
     }
 
-    function withdraw(uint256 _amount) external onlyOwner() {
+    function withdrawBFC(uint256 _amount) external onlyOwner() {
         require(address(this).balance >= _amount, "Insufficient contract balance for withdrawal");
         payable(owner()).transfer(_amount);   
     }
@@ -46,21 +43,15 @@ contract SaleContract is Ownable {
     }
 
     function BuySBTUSDC() external checkSwitch() {
-        uint256 price = priceContract.getPrice();
+        uint256 price = priceContract.getSBTPriceUSDC();
         stableContract.transferFrom(_msgSender(), address(this), price);
         tokenContract.mintSBT(_msgSender());
     }
 
     function BuySBTBFC() external payable checkSwitch() { // BFC : native token
-        uint256 price = _SBTPriceBFC();
+        uint256 price = priceContract.getSBTPriceBFC();
         require(msg.value == price, "Invalid Price");
         tokenContract.mintSBT(_msgSender());
-    }
-
-    function _SBTPriceBFC() internal view returns(uint256) {
-        uint256 Ratio = priceContract.getBFCUSDCRatio();
-        uint256 price = priceContract.getPrice();
-        return price.mulDiv(Ratio, USDCDecimals);
     }
 
 }
