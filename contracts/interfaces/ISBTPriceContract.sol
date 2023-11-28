acd2c617a95934671b48e88a067a2b1fbc493693
DoHyeon <wjs505@pi-lab.io>
2023-11-28 13:44:46 +0900

    update: ISBTPriceContract.sol

--- contracts/interfaces/ISBTPriceContract.sol
+++ contracts/interfaces/ISBTPriceContract.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface ISBTPriceContract {
-    function getPrice() external view returns(uint);
-    function getBFCUSDCRatio() external view returns (uint);
+    function getSBTPriceUSDC() external view returns(uint256);
+    function getSBTPriceBFC() external view returns(uint256);
 }
